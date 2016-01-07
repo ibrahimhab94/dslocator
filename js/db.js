d324358a304f3a0dcfbfae4eada9454837a5b535
Ibrahim Haboush <ihaboush@icloud.com>
2016-01-08 01:30:28 +0200

    w

--- js/db.js
+++ js/db.js
@@ -1,22 +1,63 @@
-/**
- * Created by ihaboush on 1/7/16.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-$(document).ready(function(){
-    var dbtable1= [["1","firstMall"],["2","secondMall"]];
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        var dbtable1= [["1","firstMall"],["2","secondMall"]];
 
-    $("#search").on("click",function(){
-        store = document.getElementById("store").value;
-        for(var i=0;i<dbtable1.length;i++){
-            console.log(dbtable1[i]+""+store);
-            if(dbtable1[i][0].localeCompare(store) == 0) {
-                document.getElementById("result").innerHTML = "Found : " + dbtable1[i][1];
-                return null;
-            }
-            document.getElementById("result").innerHTML = "Not Found !!";
+        document.getElementById("search").addEventListener("click",function(){
+            store = document.getElementById("store").value;
+            for(var i=0;i<dbtable1.length;i++){
+                console.log(dbtable1[i]+""+store);
+                if(dbtable1[i][0].localeCompare(store) == 0) {
+                    document.getElementById("result").innerHTML = "Found : " + dbtable1[i][1];
+                    return null;
+                }
+                document.getElementById("result").innerHTML = "Not Found !!";
                 console.log(dbtable1.length);
             }
-    });
-});
+        });
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-
+        console.log('Received Event: ' + id);
+    }
+};
